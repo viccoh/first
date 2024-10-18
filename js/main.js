b6d129811e51c31b4480bb06942f7962cf1ceaea
viccoh <145039403+viccoh@users.noreply.github.com>
2024-10-18 22:57:16 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,14 +7,46 @@
             }
         }, 1);
     };
-    spinner();
+    spinner(0);
     new WOW().init();
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 300) {
+        if ($(this).scrollTop() > 200) {
             $('.sticky-top').addClass('shadow-sm').css('top', '0px');
         } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-200px');
         }
+    });
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav: true,
+        navText: ['<i class="fa fa-angle-right"></i>', '<i class="fa fa-angle-left"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 2
+            },
+            1200: {
+                items: 2
+            }
+        }
+    });
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 5,
+        time: 2000
     });
     $(window).scroll(function() {
         if ($(this).scrollTop() > 300) {
@@ -28,14 +60,5 @@
             scrollTop: 0
         }, 1500, 'easeInOutExpo');
         return false;
-    });
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        items: 1,
-        dots: false,
-        loop: true,
-        nav: true,
-        navText: ['<i class="bi bi-chevron-left"></i>', '<i class="bi bi-chevron-right"></i>']
     });
 })(jQuery);
